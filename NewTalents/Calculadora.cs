176fd34c194dc4a8c5008a658497b3be5339e460
hugosdias <hugo_sdias@hotmail.com>
2023-10-08 15:14:26 -0300

    finalizando desafio

---
+++ NewTalents/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> historico;
+        private DateTime data;
+
+        public Calculadora(DateTime data)
+        {
+            historico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            historico.Insert(0, $"Resultado: {resultado} - {data}");
+            return resultado;
+
+            //return num1 + num2;
+        }
+
+        public int Subtrair( int num1 , int num2 )
+        {
+            int resultado = num1 - num2;
+            historico.Insert(0 , $"Resultado: {resultado} - {data}");
+            return resultado;
+
+        }
+
+        public int Multiplicar( int num1 , int num2 )
+        {
+            int resultado = num1 * num2;
+            historico.Insert(0 , $"Resultado: {resultado} - {data}");
+            return resultado;
+        }
+
+        public int Dividir( int num1 , int num2 )
+        {
+            int resultado = num1 / num2;
+            historico.Insert(0 , $"Resultado: {resultado} - {data}");
+            return resultado;
+        }
+
+        public List<string> Historico()
+        {
+            List<string> res;
+            historico.RemoveRange(3, historico.Count - 3);
+            return historico;
+        }
+    }
+}
---
+++ TesteNewTalents/UnitTest1.cs
@@ -0,0 +1,92 @@
+using NewTalents;
+using System;
+using Xunit;
+
+namespace TesteNewTalents
+{
+    public class UnitTest1
+    {
+        public Calculadora construirClasse()
+        {
+            DateTime data = DateTime.Now;
+            Calculadora calc = new Calculadora(data);
+
+            return calc;
+        }
+
+
+        [Theory]
+        [InlineData (1, 2 ,3)]
+        [InlineData (4, 5 ,9)]
+        public void TesteSomar(int num1, int num2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Somar(num1, num2);
+
+            Assert.Equal(resultado , resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1 , 2 , 2)]
+        [InlineData(4 , 5 , 20)]
+        public void TesteMultiplicar( int num1 , int num2 , int resultado )
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Multiplicar(num1 , num2);
+
+            Assert.Equal(resultado , resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2 , 3)]
+        [InlineData(20 , 5 , 4)]
+        public void TesteDividir( int num1 , int num2 , int resultado )
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Dividir(num1 , num2);
+
+            Assert.Equal(resultado , resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6 , 2 , 4)]
+        [InlineData(20 , 5 , 15)]
+        public void TesteSubtrair( int num1 , int num2 , int resultado )
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Subtrair(num1 , num2);
+
+            Assert.Equal(resultado , resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = construirClasse();
+
+            Assert.Throws<DivideByZeroException>(
+                () => calc.Dividir(3 , 0)
+            );
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = construirClasse();
+
+            calc.Somar(1 , 2);
+            calc.Somar(2 , 8);
+            calc.Somar(3 , 7);
+            calc.Somar(4 , 1);
+
+            var lista = calc.Historico();
+
+            Assert.NotEmpty(calc.Historico());
+            Assert.Equal(3 , lista.Count);
+        }
+    }
+}
